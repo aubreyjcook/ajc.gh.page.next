88247c545d04a35c8d56a9e0ef22171e5888662f
aubreyjcook <aubreyjcook@gmail.com>
2026-08-24 19:02:35 -0500

    update config file to use commonjs syntax

--- ajc-gh-page-next/next.config.js
+++ ajc-gh-page-next/next.config.js
@@ -1,9 +1,14 @@
-import type { NextConfig } from "next";
+// ✅ Works with all Node.js versions, no special setup needed
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "/ajc-gh-page-next",
-  output: "export",
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/ajc-gh-page-next' : '',
+  assetPrefix: isProd ? '/ajc-gh-page-next/' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
